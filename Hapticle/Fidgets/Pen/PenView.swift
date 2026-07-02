96d088ccfcb1c8aab3bac16aa3700ef45fed0d29
Syauqi Auliya (Cho) <syauqido@gmail.com>
2026-07-02 11:25:15 +0800

    [PENS] - pen placeholder images and code - background colour asset - simple sensory feedback haptic

--- Hapticle/Fidgets/Pen/PenView.swift
+++ Hapticle/Fidgets/Pen/PenView.swift
@@ -5,3 +5,30 @@
 //  Created by Syauqi Auliya M on 02/07/26.
 //
 
+import SwiftUI
+
+struct PenView: View {
+    
+    @State private var isClicked: Bool = false
+    
+    var body: some View {
+        
+        VStack {
+            Image(isClicked ? "PenPlaceholder_Clicked" : "PenPlaceholder")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 1000, height: 1000)
+                .onTapGesture {
+                    isClicked.toggle()
+                }
+                .padding(.top, 600)
+                .sensoryFeedback(isClicked ? .impact(flexibility: .solid) : .impact(flexibility: .rigid), trigger: isClicked)
+        }
+        .background(Color.backgroundColour)
+        
+    }
+}
+
+#Preview {
+    PenView()
+}
