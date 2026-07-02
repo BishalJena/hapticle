c1eb4b6dbf9c99084038e7b75627b9cb892f2066
Syauqi Auliya (Cho) <syauqido@gmail.com>
2026-07-02 12:08:00 +0800

    - update mixed media pen

--- Hapticle/Fidgets/Pen/PenView.swift
+++ Hapticle/Fidgets/Pen/PenView.swift
@@ -7,26 +7,41 @@
 
 import SwiftUI
 
-struct PenView: View {
+struct ThreeStatePenStyle: ButtonStyle {
+    @Binding var isClicked: Bool
     
+    // Optimizing the style body to minimize redrawing overhead
+    func makeBody(configuration: Configuration) -> some View {
+        let currentAsset = configuration.isPressed ? "PenV1_Clicking" : (isClicked ? "PenV1_Clicked" : "PenV1_Unclicked")
+        
+        return Image(currentAsset)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 400, height: 600)
+            .scaleEffect(configuration.isPressed ? 0.85 : 1.0, anchor: .bottom)
+            .animation(.bouncy(duration: 0.1, extraBounce: 0.1), value: configuration.isPressed)
+    }
+}
+
+struct PenView: View {
     @State private var isClicked: Bool = false
     
     var body: some View {
-        
         VStack {
-            Image(isClicked ? "PenPlaceholder_Clicked" : "PenPlaceholder")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 1000, height: 1000)
-                .onTapGesture {
-                    isClicked.toggle()
-                }
-                .padding(.top, 600)
-                .sensoryFeedback(isClicked ? .impact(flexibility: .solid) : .impact(flexibility: .rigid), trigger: isClicked)
+            Button {
+                isClicked.toggle()
+            } label: {
+                
+                Text("Toggle Pen")
+            }
+            .buttonStyle(ThreeStatePenStyle(isClicked: $isClicked))
+            .padding(.top, 300)
+            .sensoryFeedback(.impact(weight: .medium, intensity: 1.0), trigger: isClicked)
         }
         .background(Color.backgroundColour)
-        
+        .ignoresSafeArea()
     }
+    
 }
 
 #Preview {
