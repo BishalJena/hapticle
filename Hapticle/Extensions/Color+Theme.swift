e4992056adb10f7b93b7e508580f3d27f38c9971
Renoceros <130336017+Renoceros@users.noreply.github.com>
2026-07-06 20:30:13 +0800

    feat: add color assets to catalogs and define Color SwiftUI extensions

---
+++ Hapticle/Extensions/Color+Theme.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+extension Color {
+    // Light Theme Colors
+    static let primaryLight = Color("PrimaryLight")
+    static let highlightLight = Color("HighlightLight")
+    static let shadowLight = Color("ShadowLight")
+
+    // Dark Theme Colors
+    static let primaryDark = Color("PrimaryDark")
+    static let highlightDark = Color("HighlightDark")
+    static let shadowDark = Color("ShadowDark")
+
+    // Accent Theme Colors
+    static let accent = Color("Accent")
+    static let accentHighlight = Color("AccentHighlight")
+    static let accentShadow = Color("AccentShadow")
+}
